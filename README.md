7f33927d4faf1d443afb56643c0115fd5e5b5a2a
Wilbur <wilbur@wilbur.com>
2023-01-16 16:18:12 -0300

    - Working with Layouts

---
+++ README.md
@@ -0,0 +1,13 @@
+# Modern Web Applications with Go
+
+### To run
+1. Without create compiled file
+```
+$ go run cmd/web/*.go
+``` 
+
+2. Create compiled file
+```
+$ go build cmd/web/*.go
+$ ./main
+```
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	error := parsedTemplate.Execute(w, nil)
 	if error != nil {
 		fmt.Println("Error executing template :", error)
